d2710c151766fa311a3159973e63daaac4a6321f
AfzalReyaz <afjalreyaz@gmail.com>
2023-07-19 03:52:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import Signup from './psges/Signup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Header from './pages/Header';
 
 function App() {
   return (
-   <>
-   <Signup />
-   </>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<SignUp/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>this is SignUp</div>
+  )
+}
+
+export default SignUp
